d3b90151f6069c837e042530663951d398e0c0d3
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-10-03 20:06:05 +0900

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.kkomaptest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.kkomaptest"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+
+    implementation(libs.android.v260)
+    implementation(libs.v2.all)
+    implementation(libs.v2.user) // 카카오 로그인 API 모듈
+    implementation(libs.v2.share) // 카카오톡 공유 API 모듈
+    implementation(libs.v2.talk) // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation(libs.v2.friend) // 피커 API 모듈
+    implementation(libs.v2.navi) // 카카오내비 API 모듈
+    implementation(libs.v2.cert) // 카카오톡 인증 서비스 API 모듈
+
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+
+
+
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
